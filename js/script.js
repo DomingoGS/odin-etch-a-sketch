dc6d85bd75464176ac3b363fe70a93e1f3c126f6
Domingo Garcia (VM) <79866751+DomingoGS@users.noreply.github.com>
2023-04-13 12:14:42 +0200

    Add reset grid functionality to button
    Added the code for the button to open a prompt asking the user for the
    number of squares per side of the grid and using the provided number to
    reset and regenerate the grid.

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 const container = document.createElement('div');
-container.classList.add('grid-container');
+container.classList.add('container');
 container.style.display = 'flex';
 container.style.flexDirection = 'column';
 container.style.alignItems = 'center';
@@ -10,32 +10,51 @@ button.style.height = '40px';
 button.style.width = '100px';
 button.style.margin = '1rem';
 button.style.fontSize = '18px';
-//button.addEventListener('click', () => {
-//
-//});
+button.addEventListener('click', () => {
+    let squaresPerSide;
+    let invalidNumber;
+    
+    do {
+        squaresPerSide = prompt('Number of squares per side for the new grid? (max. 100)');
+        invalidNumber = squaresPerSide < 2 || squaresPerSide > 100 || !squaresPerSide;
+        if (invalidNumber) alert('Invalid number!!');
+    } while (invalidNumber);
 
+    const grid = document.getElementsByClassName('grid');
+    container.removeChild(grid[0]);
+
+    const newGrid = buildGrid(squaresPerSide);
+    container.appendChild(newGrid);
+});
 container.appendChild(button);
 
-for (row = 1; row <= 16; row++) {
-    const newRow = document.createElement('div');
-    newRow.classList.add(`row${row}`);
-    newRow.setAttribute('style', 'display: flex; height: 60px');
-    
-    for (column = 1; column <=16; column++) {
-        const newDiv = document.createElement('div');
-        newDiv.classList.add(`div${column}`);
-        newDiv.style.boxSizing = 'border-box';
-        newDiv.style.width = '60px';
-        newDiv.style.border = '1px solid black';
-        newDiv.addEventListener('mouseover', () => {
-            newDiv.style.backgroundColor = 'red';
-        });
-        newRow.appendChild(newDiv);
+function buildGrid(squaresPerSide = 16) {
+    const grid = document.createElement('div');
+    grid.classList.add('grid');
+    grid.style.border = '1px solid grey';
+    for (row = 1; row <= squaresPerSide; row++) {
+        const newRow = document.createElement('div');
+        newRow.classList.add(`row${row}`);
+        newRow.setAttribute('style', `display: flex; height: ${960 / squaresPerSide}px`);
+        
+        for (column = 1; column <=squaresPerSide; column++) {
+            const newDiv = document.createElement('div');
+            newDiv.classList.add(`div${column}`);
+            newDiv.style.boxSizing = 'border-box';
+            newDiv.style.width = `${960 / squaresPerSide}px`;
+            newDiv.addEventListener('mouseover', () => {
+                newDiv.style.backgroundColor = 'red';
+            });
+            newRow.appendChild(newDiv);
+        }
+        
+        grid.appendChild(newRow);
     }
-    
-    container.appendChild(newRow);
+    return grid;
 }
 
+const newGrid = buildGrid();
+container.appendChild(newGrid);
 const body = document.querySelector('body');
-body.appendChild(container);
 body.setAttribute('style', 'display: flex; height: 100vh; justify-content: center; align-items: center');
+body.appendChild(container);
