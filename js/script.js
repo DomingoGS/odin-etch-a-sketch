db622c676a3e2af2786cc82bd22db9fea0fab0c4
Domingo Garcia (VM) <79866751+DomingoGS@users.noreply.github.com>
2023-04-13 10:21:21 +0200

    Add trailing hover effect
    Add the JavaScript code for the grid divs to change color when the mouse
    hovers over them, leaving a trail through the grid.

--- js/script.js
+++ js/script.js
@@ -1,15 +1,25 @@
 const container = document.createElement('div');
 container.classList.add('grid-container');
 
+//const newGridBtn = document.createElement('button');
+//newGridBtn.innerText = 'New Grid';
+//container.appendChild(newGridBtn);
+
 for (row = 1; row <= 16; row++) {
     const newRow = document.createElement('div');
     newRow.classList.add(`row${row}`);
-    newRow.setAttribute('style', 'display: flex; height: 40px');
+    newRow.setAttribute('style', 'display: flex; height: 60px');
     
     for (column = 1; column <=16; column++) {
         const newDiv = document.createElement('div');
         newDiv.classList.add(`div${column}`);
-        newDiv.setAttribute('style', 'box-sizing: border-box; width: 40px; border: 1px solid black');
+        newDiv.style.boxSizing = 'border-box';
+        newDiv.style.width = '60px';
+        newDiv.style.border = '1px solid black';
+        newDiv.addEventListener('mouseover', () => {
+            console.log("Hovering...");
+            newDiv.style.backgroundColor = 'red';
+        });
         newRow.appendChild(newDiv);
     }
     
